8d3f8a7ee58bd5e7e487893b6a059f2bd628c295
Charles Jourdan <charles.jourdan@l33t-sarl.fr>
2018-07-27 14:16:49 +0200

    Adding i18n and i18n-loader

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueI18n from "vue-i18n";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueI18n);
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+	i18n: new VueI18n({
+		locale: "fr",
+		fallbackLocale: "fr"
+	}),
+	render: h => h(App)
+}).$mount("#app");
